975ddca1e162db65e11501b1a49aa6179f6b2f91
ProJulius <projulius141@gmail.com>
2024-01-08 11:06:54 +0700

    [F004] Calculate developer salary

--- NhanVienLapTrinh.dart
+++ NhanVienLapTrinh.dart
@@ -1,20 +1,9 @@
 import 'NhanVien.dart';
 
-class NhanVienLapTrinh extends NhanVien {
+class NhanVienLapTrinh extends NhanVien{
   int level;
-  NhanVienLapTrinh(
-      {required String hoTen,
-      required String ngaySinh,
-      required String soDT,
-      required String diaChi,
-      required double luong,
-      required this.level})
-      : super(
-            hoTen: hoTen,
-            ngaySinh: ngaySinh,
-            soDT: soDT,
-            diaChi: diaChi,
-            luong: luong) {
+  NhanVienLapTrinh({required String hoTen, required String ngaySinh, required String soDT, required String diaChi, required double luong, required this.level})
+      : super(hoTen: hoTen, ngaySinh: ngaySinh, soDT: soDT, diaChi: diaChi, luong: luong){
     maNV = maNV;
   }
   @override
@@ -22,11 +11,25 @@ class NhanVienLapTrinh extends NhanVien {
     super.maNV = 'LT' + _maNV.toString().padLeft(3, '0');
   }
 
+  double get realSalary{
+    if(level == 1){
+      return luong + 100;
+    }
+    else if(level == 2){
+      return luong + 200;
+    }
+    else if(level == 3){
+      return luong + 300;
+    }
+    else if(level == 4){
+      return luong + 400;
+    }
+    else {
+      return luong;
+    }
+  }
   @override
   String toString() {
-    return super.toString() + '\t$level\t';
+    return super.toString() + '\t$level' + '\t$realSalary';
   }
-
-  @override
-  double get realSalary => 0;
 }
