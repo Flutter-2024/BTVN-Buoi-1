634b5a51f9f9b4ef7eeed63315f7d84d53c1bd1c
meixsosimp <linhmyx2003@gmail.com>
2024-01-09 18:30:06 +0700

    [F002] merge with develop

--- NhanVienLapTrinh.dart
+++ NhanVienLapTrinh.dart
@@ -2,21 +2,23 @@ import 'NhanVien.dart';
 
 class NhanVienLapTrinh extends NhanVien {
   int level;
+
   NhanVienLapTrinh(
       {required String hoTen,
         required String ngaySinh,
         required String soDT,
         required String diaChi,
         required double luong,
-        required this.level})
+        required this.level,})
       : super(
-      hoTen: hoTen,
-      ngaySinh: ngaySinh,
-      soDT: soDT,
-      diaChi: diaChi,
-      luong: luong) {
+    hoTen: hoTen,
+    ngaySinh: ngaySinh,
+    soDT: soDT,
+    diaChi: diaChi,
+    luong: luong,) {
     maNV = maNV;
   }
+
   @override
   set maNV(String _maNV) {
     super.maNV = 'LT' + _maNV.toString().padLeft(3, '0');
